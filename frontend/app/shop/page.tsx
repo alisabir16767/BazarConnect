f2dfcc228312f3aedfa4ad7a621e01c24bd3dc60
Sabir Ali <alisabir167167@gmail.com>
2025-04-05 18:31:23 +0530

    dialog box tested and data send to the server

--- frontend/app/shop/page.tsx
+++ frontend/app/shop/page.tsx
@@ -4,7 +4,33 @@ import React from "react";
 export default function NewShopPage() {
   return (
    <>
-    <h1>Add shop under creation</h1>
+   <form action="">
+       
+        <div className="flex flex-col gap-4">
+          <label htmlFor="shopName">Shop Name</label>
+          <input
+            type="text"
+            id="shopName"
+            name="shopName"
+            placeholder="Enter your shop name"
+            className="border p-2 rounded"
+          />
+        </div>
+  
+        <div className="flex flex-col gap-4">
+          <label htmlFor="shopDescription">Shop Description</label>
+          <textarea
+            id="shopDescription"
+            name="shopDescription"
+            placeholder="Enter a brief description of your shop"
+            className="border p-2 rounded"
+          ></textarea>
+        </div>
+  
+        <button type="submit" className="bg-blue-500 text-white p-2 rounded">
+          Create Shop
+        </button>
+   </form>
    </>
   )
 }
