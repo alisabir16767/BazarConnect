0224c572b75414f30abda450cf4376f7a8aea2d5
Sabir Ali <alisabir167167@gmail.com>
2025-04-12 14:08:04 +0530

    COMMENTS REMOVED FROM CART

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -2,16 +2,9 @@ const express = require("express");
 const cartController = require("../controllers/cartController");
 const router = express.Router();
 
-// Route to get user's cart
 router.get("/:userId", cartController.getAllCart);
-
-// Route to add a product to user's cart
 router.post("/:userId/add", cartController.addToCart);
-
-// Route to update a product's quantity in the cart
 router.put("/:userId/update", cartController.updateCart);
-
-// Route to remove a product from user's cart
 router.delete("/:userId/remove/:productId", cartController.deleteCart);
 
 module.exports = router;
