c67ef9e2ea35c4a070f19913f5b47cfa775b7f0b
Sabir Ali <alisabir167167@gmail.com>
2025-04-16 21:33:03 +0530

    ,jshdmnx

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -5,7 +5,7 @@ const { isAuthenticated } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.get("/", isAuthenticated, cartController.getAllCart);
+router.get("/cart", isAuthenticated, cartController.getAllCart);
 router.post("/add", isAuthenticated, cartController.addToCart);
 router.put("/update", isAuthenticated, cartController.updateCart);
 router.delete("/remove/:productId", isAuthenticated, cartController.deleteCart);
