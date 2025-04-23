a5350d5f3944ef4029f693c002c76ef2bbde5736
Sabir Ali <alisabir167167@gmail.com>
2025-04-23 16:15:00 +0530

    cart fixed for production

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
+// Make sure this path is correct
 const cartController = require("../controllers/cartController");
-console.log("cartController", cartController);
 const { isAuthenticated } = require("../middleware/authMiddleware");
-
 const router = express.Router();
+
+console.log("Controller functions:", Object.keys(cartController));
 
 router.get("/cart", isAuthenticated, cartController.getAllCart);
 router.post("/add", isAuthenticated, cartController.addToCart);
