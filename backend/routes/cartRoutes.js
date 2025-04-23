bb60c784f3ea06205e8877516dd24fc59c9390ba
Sabir Ali <alisabir167167@gmail.com>
2025-04-23 16:20:38 +0530

    submitted middleware

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,14 +1,18 @@
 const express = require("express");
 // Make sure this path is correct
 const cartController = require("../controllers/cartController");
-const { isAuthenticated } = require("../middleware/authMiddleware");
+const { ensureAuthenticated } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 console.log("Controller functions:", Object.keys(cartController));
 
-router.get("/cart", isAuthenticated, cartController.getAllCart);
-router.post("/add", isAuthenticated, cartController.addToCart);
-router.put("/update", isAuthenticated, cartController.updateCart);
-router.delete("/remove/:productId", isAuthenticated, cartController.deleteCart);
+router.get("/cart", ensureAuthenticated, cartController.getAllCart);
+router.post("/add", ensureAuthenticated, cartController.addToCart);
+router.put("/update", ensureAuthenticated, cartController.updateCart);
+router.delete(
+  "/remove/:productId",
+  ensureAuthenticated,
+  cartController.deleteCart
+);
 
 module.exports = router;
